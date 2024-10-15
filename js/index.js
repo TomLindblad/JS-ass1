a9e418440bf147241c8c7e979de0fb0edf1d2d05
TomLindblad <168995337+TomLindblad@users.noreply.github.com>
2024-10-15 10:14:03 +0200

    made code

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// EXERCISE 1:  
+
+console.log("Hello, World!");
+
+
+// EXERCISE 2:
+
+let myName = "Tom";
+let myAge = 35;
+let myFavColour = "Green"
+
+//console.log("My name is " + myName + ", maybe I'm " + myAge + " years old. Maybe I'm not. Go team " + myFavColour)
+console.log(`My name is ${myName} maybe I'm ${myAge} years old. Maybe I'm not. Go team ${myFavColour}`)
+
+
+// EXERCISE 3:
+
+let num1;
+let num2;
+
+num1 = 3;
+num2 = 2; 
+
+console.log(num1 + num2);
